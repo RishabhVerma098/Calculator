3cdfce6778749a75f2555a7b0674a602c3de282c
rishabh verma <vermarishabh0987@gmail.com>
2019-01-19 20:13:50 +0530

    Now completing my basic calculator ..

--- app/src/main/java/com/example/verma0987/calculator/MainActivity.kt
+++ app/src/main/java/com/example/verma0987/calculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.verma0987.calculator
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -22,22 +23,43 @@ class MainActivity : AppCompatActivity() {
         result.text = (a+b).toString()
     }
 
+    fun mul(view: View){
+        var a = num1.text.toString().toInt()
+        var b = num2.text.toString().toInt()
+        result.text = (a*b).toString()
 
-
-
-
-
-
-
-
-
-    fun commit(): Int {
-        println("Hello calulator")
-        val a:Int = 10
-        val b:Int = 9
-        val c = a+b
-        return c
     }
+    fun div(view: View){
+        var a = num1.text.toString().toFloat()
+        var b = num2.text.toString().toFloat()
+        val message:String = "yo deno cannot be zero"
+
+        try {
+            result.text = (a/b).toString()
+        }catch (e:ArithmeticException){
+            Toast.makeText(this , message, Toast.LENGTH_SHORT).show()
+            num2.text.clear()
+        }
+
+
+
+    }
+    fun sub(view: View){
+        var a = num1.text.toString().toInt()
+        var b = num2.text.toString().toInt()
+        result.text = (a-b).toString()
+
+    }
+
+
+
+
+
+
+
+
+
+
 
 
 }
