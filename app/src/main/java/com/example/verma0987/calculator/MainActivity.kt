7ce4bdde005691632773dee89efb4ced18a7e328
rishabh verma <vermarishabh0987@gmail.com>
2019-01-19 14:31:34 +0530

    My Second change to Calculator to how github integration work

--- app/src/main/java/com/example/verma0987/calculator/MainActivity.kt
+++ app/src/main/java/com/example/verma0987/calculator/MainActivity.kt
@@ -8,10 +8,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        commit()
+        val R = commit()
+        println("A+B = $R")
+
     }
 
-    fun commit(){
+    fun commit(): Int {
         println("Hello calulator")
+        val a:Int = 10
+        val b:Int = 9
+        val c = a+b
+        return c
     }
+
+
 }
